e05bf460f191487df211eef2bdf3bf2bd39fb8a4
Chris N <awedeven@gmail.com>
2025-09-24 00:15:05 -0700

    Add some JSON logging for testing stuffˆ

--- LightFixture.SourceGeneration/LightFixture.SourceGeneration.Sample/Program.cs
+++ LightFixture.SourceGeneration/LightFixture.SourceGeneration.Sample/Program.cs
@@ -1,13 +1,18 @@
-
 using System;
+using System.Text.Json;
 using LightFixture;
 using LightFixture.SourceGeneration.Sample;
 
 var dataProvider = new DataProviderBuilder()
     .Customize(new SampleDataProvider())
     .Build();
-    
-var data = dataProvider.Resolve<SampleDataOuter>();
-var data2 = dataProvider.Resolve<SampleDataInner>();
 
-Console.WriteLine();
+var data = dataProvider.Resolve<SampleDataOuter>();
+
+var json = JsonSerializer.Serialize(
+    data,
+    new JsonSerializerOptions()
+    {
+        WriteIndented = true,
+    });
+Console.WriteLine(json);
