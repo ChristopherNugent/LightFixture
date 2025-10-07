0788b7215b420fb76e9f556c3d97484a964bb170
Chris N <ChristopherTNugent@proton.me>
2025-10-06 20:44:53 -0700

    Suppress warnings of unused parameters on source generator target attributes

--- LightFixture/DataFactoryAttribute.cs
+++ LightFixture/DataFactoryAttribute.cs
@@ -1,3 +1,4 @@
+#pragma warning disable CS9113 // Parameter is unread - This is just a target for a source generator.
 namespace LightFixture;
 
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
