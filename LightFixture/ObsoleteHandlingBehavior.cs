59ca70f97dd1049d02b7d67c2abd2578e2e1a4c4
chris <ChristopherTNugent@proton.me>
2026-05-03 22:48:12 -0700

    Add some documentation

--- LightFixture/ObsoleteHandlingBehavior.cs
+++ LightFixture/ObsoleteHandlingBehavior.cs
@@ -2,8 +2,23 @@ namespace LightFixture;
 
 public enum ObsoleteHandlingBehavior
 {
+    /// <summary>
+    /// No special handling.
+    /// </summary>
     None,
+    
+    /// <summary>
+    /// Ignore obsolete properties when generating factories.
+    /// </summary>
     IgnoreObsolete,
+    
+    /// <summary>
+    /// Suppress warnings from obsolete members in generated code.
+    /// </summary>
     SuppressWarnings,
+    
+    /// <summary>
+    /// Suppress errors from obsolete members in generated code.
+    /// </summary>
     SuppressErrors,
 }
